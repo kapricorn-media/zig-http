46fb36c47c047063d6aec3ac164a94311d6e490b
Jose Rico <jmrico01@gmail.com>
2021-10-13 19:17:22 -0400

    rename libhttp -> server

--- src/test.zig
+++ src/test.zig
@@ -5,9 +5,8 @@ const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const expectEqualSlices = std.testing.expectEqualSlices;
 
-const libhttp = @import("libhttp.zig");
-
-const cc = @import("client.zig");
+const client = @import("client.zig");
+const server = @import("server.zig");
 
 const TEST_PORT = 19191;
 const TEST_HOSTNAME = "127.0.0.1";
@@ -34,13 +33,13 @@ fn expectAnyError(result: anytype) !void
 test "server, no SSL"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
     
     try expect(!gpa.deinit()); // TODO ideally this should be defer'd at the start
 }
 
-fn handlerNullData(connection: *libhttp.mg_connection, data: ?*c_void) !void
+fn handlerNullData(connection: *server.mg_connection, data: ?*c_void) !void
 {
     _ = connection;
     try expectEqual(@as(?*c_void, null), data);
@@ -49,10 +48,10 @@ fn handlerNullData(connection: *libhttp.mg_connection, data: ?*c_void) !void
 test "server, no SSL, request handler"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
 
-    libhttp.setRequestHandler(context, "/", handlerNullData, null);
+    server.setRequestHandler(context, "/", handlerNullData, null);
     
     try expect(!gpa.deinit());
 }
@@ -61,13 +60,13 @@ test "server, SSL, no cert, FAIL"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = libhttp.start(TEST_PORT, true, "", &gpa.allocator);
+    const context = server.start(TEST_PORT, true, "", &gpa.allocator);
     try expectAnyError(context);
 }
 
 // server+client tests
 
-fn expectHeaders(expected: []const cc.Header, actual: []const cc.Header) !void
+fn expectHeaders(expected: []const client.Header, actual: []const client.Header) !void
 {
     if (actual.len != expected.len) {
         std.log.err("header len actual {}, expected {}", .{actual.len, expected.len});
@@ -80,7 +79,7 @@ fn expectHeaders(expected: []const cc.Header, actual: []const cc.Header) !void
     }
 }
 
-fn handlerNoResponse(connection: *libhttp.mg_connection, data: ?*c_void) !void
+fn handlerNoResponse(connection: *server.mg_connection, data: ?*c_void) !void
 {
     _ = connection;
     try expectEqual(@as(?*c_void, null), data);
@@ -90,85 +89,85 @@ test "server+client, get /, no response"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/", handlerNoResponse, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/", handlerNoResponse, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try expectAnyError(cc.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response));
+    var response: client.Response = undefined;
+    try expectAnyError(client.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response));
 }
 
-fn handlerOk(connection: *libhttp.mg_connection, data: ?*c_void) !void
+fn handlerOk(connection: *server.mg_connection, data: ?*c_void) !void
 {
     try expectEqual(@as(?*c_void, null), data);
-    try libhttp.writeHttpCode(connection, ._200);
-    try libhttp.writeHttpEndHeader(connection);
+    try server.writeHttpCode(connection, ._200);
+    try server.writeHttpEndHeader(connection);
 }
 
 test "server+client, get /, 200"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/", handlerOk, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/", handlerOk, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
+    var response: client.Response = undefined;
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 200), response.code);
     try expectEqualSlices(u8, "OK", response.message);
     try expectEqual(@as(usize, 0), response.body.len);
 }
 
-fn handlerInternalError(connection: *libhttp.mg_connection, data: ?*c_void) !void
+fn handlerInternalError(connection: *server.mg_connection, data: ?*c_void) !void
 {
     try expectEqual(@as(?*c_void, null), data);
-    try libhttp.writeHttpCode(connection, ._500);
-    try libhttp.writeHttpEndHeader(connection);
+    try server.writeHttpCode(connection, ._500);
+    try server.writeHttpEndHeader(connection);
 }
 
 test "server+client, get /, 500"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/", handlerInternalError, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/", handlerInternalError, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
+    var response: client.Response = undefined;
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 500), response.code);
     try expectEqualSlices(u8, "Internal Server Error", response.message);
     try expectEqual(@as(usize, 0), response.body.len);
 }
 
-fn handlerHelloWorld(connection: *libhttp.mg_connection, data: ?*c_void) !void
+fn handlerHelloWorld(connection: *server.mg_connection, data: ?*c_void) !void
 {
     try expectEqual(@as(?*c_void, null), data);
-    try libhttp.writeHttpCode(connection, ._200);
-    try libhttp.writeHttpContentType(connection, .TextPlain);
-    try libhttp.writeHttpEndHeader(connection);
+    try server.writeHttpCode(connection, ._200);
+    try server.writeHttpContentType(connection, .TextPlain);
+    try server.writeHttpEndHeader(connection);
     const str = "Hello, world!";
-    try expectEqual(@as(c_int, str.len), libhttp.mg_write(connection, &str[0], str.len));
+    try expectEqual(@as(c_int, str.len), server.mg_write(connection, &str[0], str.len));
 }
 
 test "server+client, get /, 200, return data"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/", handlerHelloWorld, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/", handlerHelloWorld, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
+    var response: client.Response = undefined;
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 200), response.code);
     try expectEqualSlices(u8, "OK", response.message);
-    const expectedHeaders = [_]cc.Header {
+    const expectedHeaders = [_]client.Header {
         .{
             .name = "Content-Type",
             .value = "text/plain",
@@ -182,16 +181,16 @@ test "server+client, get /custom_uri, 200, return data"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/custom_uri", handlerHelloWorld, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/custom_uri", handlerHelloWorld, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri", &gpa.allocator, &responseData, &response);
+    var response: client.Response = undefined;
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 200), response.code);
     try expectEqualSlices(u8, "OK", response.message);
-    const expectedHeaders = [_]cc.Header {
+    const expectedHeaders = [_]client.Header {
         .{
             .name = "Content-Type",
             .value = "text/plain",
@@ -205,13 +204,13 @@ test "server+client, get /, serve /custom_uri"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/custom_uri", handlerHelloWorld, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/custom_uri", handlerHelloWorld, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
+    var response: client.Response = undefined;
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 404), response.code);
     try expectEqualSlices(u8, "Not Found", response.message);
 }
@@ -220,16 +219,16 @@ test "server+client, get /custom_uri, serve /"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/", handlerHelloWorld, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/", handlerHelloWorld, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri", &gpa.allocator, &responseData, &response);
+    var response: client.Response = undefined;
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 200), response.code);
     try expectEqualSlices(u8, "OK", response.message);
-    const expectedHeaders = [_]cc.Header {
+    const expectedHeaders = [_]client.Header {
         .{
             .name = "Content-Type",
             .value = "text/plain",
@@ -243,18 +242,18 @@ test "server+client, get /custom_uri, serve / and /custom_uri"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
-    defer libhttp.stop(context);
-    libhttp.setRequestHandler(context, "/", handlerHelloWorld, null);
-    libhttp.setRequestHandler(context, "/custom_uri", handlerNoResponse, null);
+    const context = try server.start(TEST_PORT, false, "", &gpa.allocator);
+    defer server.stop(context);
+    server.setRequestHandler(context, "/", handlerHelloWorld, null);
+    server.setRequestHandler(context, "/custom_uri", handlerNoResponse, null);
 
     var responseData: std.ArrayList(u8) = undefined;
-    var response: cc.Response = undefined;
+    var response: client.Response = undefined;
 
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 200), response.code);
     try expectEqualSlices(u8, "OK", response.message);
-    const expectedHeaders = [_]cc.Header {
+    const expectedHeaders = [_]client.Header {
         .{
             .name = "Content-Type",
             .value = "text/plain",
@@ -263,11 +262,11 @@ test "server+client, get /custom_uri, serve / and /custom_uri"
     try expectHeaders(&expectedHeaders, response.headers[0..response.numHeaders]);
     try expectEqualSlices(u8, "Hello, world!", response.body);
 
-    try cc.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri2", &gpa.allocator, &responseData, &response);
+    try client.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri2", &gpa.allocator, &responseData, &response);
     try expectEqual(@as(u32, 200), response.code);
     try expectEqualSlices(u8, "OK", response.message);
     try expectHeaders(&expectedHeaders, response.headers[0..response.numHeaders]);
     try expectEqualSlices(u8, "Hello, world!", response.body);
 
-    try expectAnyError(cc.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri", &gpa.allocator, &responseData, &response));
+    try expectAnyError(client.get(false, TEST_PORT, TEST_HOSTNAME, "/custom_uri", &gpa.allocator, &responseData, &response));
 }
