e24773338b49a5c325ecf4dc92dffde32d09d8a4
Jose Rico <jmrico01@gmail.com>
2021-10-12 20:49:30 -0400

    factor out port, wait 1 second before tests

--- src/test.zig
+++ src/test.zig
@@ -10,10 +10,12 @@ const libhttp = @import("libhttp.zig");
 const c = @cImport(@cInclude("unistd.h"));
 const cc = @import("client.zig");
 
-test "success: port 9000, no SSL"
+const TEST_PORT = 19191;
+
+test "server, no SSL"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const context = try libhttp.start(9000, false, "", &gpa.allocator);
+    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
     defer libhttp.stop(context);
     
     try expect(!gpa.deinit()); // TODO ideally this should be defer'd at the start
@@ -25,10 +27,10 @@ fn handlerNullData(connection: *libhttp.mg_connection, data: ?*c_void) !void
     try expectEqual(data, null);
 }
 
-test "success: port 9000, no SSL, request handler"
+test "server, no SSL, request handler"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const context = try libhttp.start(9000, false, "", &gpa.allocator);
+    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
     defer libhttp.stop(context);
 
     libhttp.setRequestHandler(context, "/", handlerNullData, null);
@@ -36,11 +38,11 @@ test "success: port 9000, no SSL, request handler"
     try expect(!gpa.deinit());
 }
 
-test "fail: port 9000, SSL, no cert"
+test "server, SSL, no cert, FAIL"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const context = libhttp.start(9000, true, "", &gpa.allocator);
+    const context = libhttp.start(TEST_PORT, true, "", &gpa.allocator);
     _ = context catch {
         try expect(!gpa.deinit());
         return;
@@ -90,15 +92,15 @@ fn handlerNoResponse(connection: *libhttp.mg_connection, data: ?*c_void) !void
 test "http localhost, no response"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const port = 9000;
 
-    const context = try libhttp.start(port, false, "", &gpa.allocator);
+    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
     defer libhttp.stop(context);
     libhttp.setRequestHandler(context, "/", handlerNoResponse, null);
+    _ = c.sleep(1);
 
     var responseData: std.ArrayList(u8) = undefined;
     var response: cc.Response = undefined;
-    cc.get(false, port, "localhost", "/", &gpa.allocator, &responseData, &response) catch {
+    cc.get(false, TEST_PORT, "localhost", "/", &gpa.allocator, &responseData, &response) catch {
         try expect(!gpa.deinit());
         return;
     };
@@ -116,15 +118,15 @@ fn handlerOk(connection: *libhttp.mg_connection, data: ?*c_void) !void
 test "http localhost, 200 OK"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const port = 9000;
 
-    const context = try libhttp.start(port, false, "", &gpa.allocator);
+    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
     defer libhttp.stop(context);
     libhttp.setRequestHandler(context, "/", handlerOk, null);
+    _ = c.sleep(1);
 
     var responseData: std.ArrayList(u8) = undefined;
     var response: cc.Response = undefined;
-    try cc.get(false, port, "localhost", "/", &gpa.allocator, &responseData, &response);
+    try cc.get(false, TEST_PORT, "localhost", "/", &gpa.allocator, &responseData, &response);
     try expectEqual(response.code, 200);
     try expectEqualSlices(u8, response.message, "OK");
     try expectEqual(response.body.len, 0);
@@ -140,15 +142,15 @@ fn handlerInternalError(connection: *libhttp.mg_connection, data: ?*c_void) !voi
 test "http localhost, 500"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const port = 9000;
 
-    const context = try libhttp.start(port, false, "", &gpa.allocator);
+    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
     defer libhttp.stop(context);
     libhttp.setRequestHandler(context, "/", handlerInternalError, null);
+    _ = c.sleep(1);
 
     var responseData: std.ArrayList(u8) = undefined;
     var response: cc.Response = undefined;
-    try cc.get(false, port, "localhost", "/", &gpa.allocator, &responseData, &response);
+    try cc.get(false, TEST_PORT, "localhost", "/", &gpa.allocator, &responseData, &response);
     try expectEqual(response.code, 500);
     try expectEqualSlices(u8, response.message, "Internal Server Error");
     try expectEqual(response.body.len, 0);
@@ -166,15 +168,15 @@ fn handlerHelloWorld(connection: *libhttp.mg_connection, data: ?*c_void) !void
 test "http localhost, 200, return data"
 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const port = 9000;
 
-    const context = try libhttp.start(port, false, "", &gpa.allocator);
+    const context = try libhttp.start(TEST_PORT, false, "", &gpa.allocator);
     defer libhttp.stop(context);
     libhttp.setRequestHandler(context, "/", handlerHelloWorld, null);
+    _ = c.sleep(1);
 
     var responseData: std.ArrayList(u8) = undefined;
     var response: cc.Response = undefined;
-    try cc.get(false, port, "localhost", "/", &gpa.allocator, &responseData, &response);
+    try cc.get(false, TEST_PORT, "localhost", "/", &gpa.allocator, &responseData, &response);
     try expectEqual(response.code, 200);
     try expectEqualSlices(u8, response.message, "OK");
     try expectEqualSlices(u8, response.body, "Hello, world!");
